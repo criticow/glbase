09ef6e18cb1e404639dd6f1439527f1422d69f03
Iury Ferreira <criticow@outlook.com>
2024-02-02 07:46:49 -0300

    removed glm headers

--- src/glbpch.hpp
+++ src/glbpch.hpp
@@ -24,11 +24,7 @@
 
 // Math
 #include <glm/glm.hpp>
-#include <glm/gtc/quaternion.hpp>
-#include <glm/gtx/euler_angles.hpp>
 
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
-
-// #define LOGGER_GLM_EXT
